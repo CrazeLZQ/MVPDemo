fd3b263cd58c5729e2949dccd60f3901ca7d7277
CrazeLZQ <1029008941@qq.com>
2017-12-20 10:39:57 +0800

    Revert "20171220"
    This reverts commit dad9bd1dbb3a87fb650ce107c84d542d2bee4eb9.

--- app/src/main/java/com/lzq/mvp/ui/MainActivity.java
+++ app/src/main/java/com/lzq/mvp/ui/MainActivity.java
@@ -1,28 +1,25 @@
-package com.lzq.mvp.ui.fragment;
+package com.lzq.mvp.ui;
 
 import android.view.View;
 import android.widget.TextView;
-
 import com.lzq.mvp.R;
-import com.lzq.mvp.base.BaseFragment;
+import com.lzq.mvp.base.BaseActivity;
 import com.lzq.mvp.presenter.Presenter_1;
 import com.lzq.mvp.ui.view.View_1;
 
-/**
- * Created by LZQ on 2017/12/20 10:09.
- * Describe :
- */
-
-public class TestFragment extends BaseFragment<View_1,Presenter_1> implements View_1{
+public class MainActivity extends BaseActivity<View_1,Presenter_1> implements View_1{
 
     private TextView mGetData,mShowData;
 
     @Override
-    public View initViews() {
-        View view = View.inflate(mActivity, R.layout.fragment_text,null);
-        mGetData = (TextView) view.findViewById(R.id.tv_3);
-        mShowData = (TextView) view.findViewById(R.id.tv_4);
-        return view;
+    protected void setRootView() {
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void initView() {
+        mGetData = (TextView) findViewById(R.id.tv_1);
+        mShowData = (TextView) findViewById(R.id.tv_2);
     }
 
     @Override
@@ -32,11 +29,11 @@ public class TestFragment extends BaseFragment<View_1,Presenter_1> implements Vi
 
     @Override
     protected View_1 createView() {
-        return this;
+        return MainActivity.this;
     }
 
     @Override
-    public void initData() {
+    protected void initData() {
         mGetData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
